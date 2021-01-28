3f4a6f3e2929b4a297ae3f38ac7ef1341b72fe9e
TheHamkerCat <thehamkercat@gmail.com>
2021-01-28 18:07:14 +0530

    This should work until december 2013.

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 ## Add all meetings you want the bot to join in below list.
 ## Example: meeting_links = [link1, link2, link3]
+## if there is only one meeting, meeting_links = [link]
 
 meeting_links = ['https://us04web.zoom.us/j/71382840904?pwd=UlJia1dxTmZaT09', 'https://us04web.zoom.us/j/7134?pwd=U09A5S4D65']
 
 ## Add the delay between the meetings below.
 ## bot will sleep for 'n' number of seconds before joining other meetings.
 ## Example: delay = 3600, so bot will sleep for one hour
+## if there is only one meeting, set delay value to 0
 
 delay = 3000
