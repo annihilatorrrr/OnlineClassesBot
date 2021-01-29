f66075f3083983ebff32ff8864fe293b765ec407
TheHamkerCat <thehamkercat@gmail.com>
2021-01-29 16:05:11 +0530

    smol phix by hamkurket

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+# Only Fill these details if you want to use the bot in terminal
+
+
 ## Add all meetings you want the bot to join in below list.
 ## Example: meeting_links = [link1, link2, link3]
 ## if there is only one meeting, meeting_links = [link]
@@ -10,3 +13,9 @@ meeting_links = ['https://us04web.zoom.us/j/71382840904?pwd=UlJia1dxTmZaT09', 'h
 ## if there is only one meeting, set delay value to 0
 
 delay = 3000
+
+
+# only fill details below if you want to use this in telegram bot mode
+
+owner_id = 1057450432
+bot_token = "11665543:AS4FJQ63AEF54GAE6U87AE657AY34R"
--- zoom_bot.py
+++ zoom_bot.py
@@ -1,22 +1,72 @@
 import time
 import keyboard
+import os
 from selenium import webdriver
-from config import delay, meeting_links as links
 
 class zoom_bot:
-	def join_meeting(self,link):
-#		self.bot = webdriver.Chrome("chromedriver.exe")
-#		self.bot = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-		self.bot.get(link)
-		time.sleep(5)
-		keyboard.send("tab", do_press=True, do_release=True)
-		keyboard.send("tab", do_press=True, do_release=True)
-		keyboard.send("enter", do_press=True, do_release=True)
-		time.sleep(10)
-		self.bot.quit()
+    def join_meeting(self,link):
+        try:
+            if os.name == 'posix':
+                self.bot = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+            else:
+                self.bot = webdriver.Chrome("chromedriver.exe")
+            self.bot.get(link)
+            time.sleep(5)
+            keyboard.send("tab", do_press=True, do_release=True)
+            keyboard.send("tab", do_press=True, do_release=True)
+            keyboard.send("enter", do_press=True, do_release=True)
+            time.sleep(10)
+            self.bot.quit()
+        except Exception as e:
+            print(str(e))
 
+run_mode = int(input(" Choose A Running Mode:\n 1. Terminal\n 2. Telegram Bot\n > "))
 bot = zoom_bot()
-for link in links:
-    bot.join_meeting(link)
-    if delay > 1:
-        time.sleep(delay)
+
+
+if run_mode == 1:
+    from config import delay, meeting_links as links
+    for link in links:
+        bot.join_meeting(link)
+        if delay > 1:
+            time.sleep(delay)
+elif run_mode == 2:
+    from pyrogram import Client, filters
+    from config import bot_token, owner_id
+    app = Client(':memory:',
+            bot_token=bot_token,
+            api_id=6,
+            api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+
+
+    @app.on_message(filters.command('start'))
+    def start(_, message):
+        message.reply_text("Hi, I'm Zoom Bot, Send Help To Get Commands.")
+
+
+    @app.on_message(filters.command('help'))
+    def help(_, message):
+        message.reply_text("/join <<link>> - To Join A Meeting")
+
+
+    @app.on_message(filters.command('join') & filters.user(owner_id))
+    def joinbot(_, message):
+        if len(message.command) != 2:
+            message.reply_text("/join Requires A Meeting Link As Argument.")
+            return
+
+        if message.entities[1]['type'] != 'url':
+            message.reply_text("/join Requires A Meeting Link As Argument.")
+            return
+        
+        link = message.command[1]
+        m = message.reply_text("Joined Meeting.")
+        try:
+            bot.join_meeting(link)
+        except Exception as e:
+            m.edit(str(e))
+            print(str(e))
+
+    app.run()
+else:
+    print("No Input, detected, Exiting!")
