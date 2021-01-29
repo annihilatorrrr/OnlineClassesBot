318f2e82624bed4cff28a9c31667349fde548ffd
Kritarth <74340401+Kody-K@users.noreply.github.com>
2021-01-29 13:32:14 +0530

    F

--- zoom_bot.py
+++ zoom_bot.py
@@ -4,10 +4,10 @@ from selenium import webdriver
 from config import delay, meeting_links as links
 
 class zoom_bot:
-	def join_meeting(self,meeting_link):
+	def join_meeting(self,link):
 #		self.bot = webdriver.Chrome("chromedriver.exe")
 #		self.bot = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-		self.bot.get(meeting_link)
+		self.bot.get(link)
 		time.sleep(5)
 		keyboard.send("tab", do_press=True, do_release=True)
 		keyboard.send("tab", do_press=True, do_release=True)
